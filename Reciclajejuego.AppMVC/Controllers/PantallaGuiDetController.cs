d0e2c5f48c83814e0e0febf79618cb7a34df768b
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-03-07 16:38:02 -0600

    Pantalla de detalles

---
+++ Reciclajejuego.AppMVC/Controllers/PantallaGuiDetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reciclajejuego.AppMVC.Controllers
+{
+    public class PantallaGuiDetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();6
+        }
+    }
+}
