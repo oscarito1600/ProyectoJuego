a60cc86b25fedfceb4aac1896a8361d1033ebc49
Daniela Campos <danielaroblox1617@gmail.com>
2026-03-20 11:21:08 -0600

    Controlador para Roles

---
+++ Reciclajejuego.AppMVC/Models/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reciclajejuego.AppMVC.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del rol es obligatorio")]
+        public string Nombre { get; set; }
+    }
+}
