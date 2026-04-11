61c80fe5b0ed837cc9c081c5802b4f1a16bac2fb
Daniela Campos <danielaroblox1617@gmail.com>
2026-04-11 17:20:34 -0600

    Se creo el nuevo modo de juego modo4 con su controlador y vista

---
+++ Reciclajejuego.AppMVC/Controllers/Modo4Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reciclajejuego.AppMVC.Controllers
+{
+    public class Modo4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
