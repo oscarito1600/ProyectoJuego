80026344c6ff62ddc8028a0b6447b244e96c02bd
Daniela Campos <danielaroblox1617@gmail.com>
2026-04-11 16:41:06 -0600

    Se creó el controlador y vista del juego modo3

---
+++ Reciclajejuego.AppMVC/Controllers/Modo3Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reciclajejuego.AppMVC.Controllers
+{
+    public class Modo3Controller : Controller
+    {
+        public IActionResult Modo3()
+        {
+            return View("Index");
+        }
+    }
+}
